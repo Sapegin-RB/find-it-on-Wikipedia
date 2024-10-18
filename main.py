43ea61c0ac4123c32e930ea884a63c9854b55b68
Sapegin-RB <sapeginskal@mail.ru>
2024-10-18 16:12:59 +0700

    the selenium team

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from selenium import webdriver
 import time # Модуль позволяет делать задержки в программе
 browser = webdriver.Firefox() # Создаем объект нашего браузера
 browser.get("https://en.wikipedia.org/wiki/Document_Object_Model") # В браузере заходим на указанную страницу
-time.sleep(10) # Задержка открытого окна 10 сек.
-browser.quit() # Команда закрывающая окно сайта
+browser.save_screenshot("dom.png") # Команда делает скриншет открытой страници
+time.sleep(5) # Задержка открытого окна 10 сек.
+browser.get("https://ru.wikipedia.org/wiki/Selenium")
+browser.save_screenshot("selenium.png") # Команда делает скриншет открытой страници
+time.sleep(3)
+browser.refresh()
