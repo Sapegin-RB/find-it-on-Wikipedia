04db71fea433424121614f10d33dcb31c73c2b5c
Sapegin-RB <sapeginskal@mail.ru>
2024-10-18 17:38:35 +0700

    a random article

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver import Keys # Команда для ввода текста с клавиатуры
 from selenium.webdriver.common.by import By # для поиска различных элементов на странице через дом
-
+import random
 import time
 
 browser = webdriver.Firefox()
 browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
 
-paragraphs = browser.find_elements(By.TAG_NAME,"p")
+hatnotes = []
 
-for paragraph in paragraphs:
-    print(paragraph.text)
-    input()
+for element in browser.find_elements(By.TAG_NAME, "div"):
+    cl = element.get_attribute("class")
+    if cl == "hatnote navigation-not-searchable":
+        hatnotes.append(element)
 
+print(hatnotes)
+hatnote = random.choice(hatnotes)
+link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+browser.get(link)
